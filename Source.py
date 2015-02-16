1a77d756721f40ce14eaafd3023f4822620a6c1d
7ider5 <7ider5@gmail.com>
2015-02-17 00:01:56 +0100

    BlinkLed v0

--- Source.py
+++ Source.py
@@ -2,28 +2,38 @@ __author__ = 'EBJ501'
 
 # General dependency declarations
 import os
-import time
+from time import time
+from time import timezone
 # Raspi GPIO dependency declarations
+import RPi.GPIO as GPIO
+#Configure GPIO 7 AS output
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT, initial = GPIO.LOW)
 
-# Production database loading
-# Data format :|Timestamp (POSIX)|Prod (Wh/min)|
-# Separator : ";"
+# Production database loading -- Data format : Timestamp (POSIX) ; Prod (Wh/min) -- Separator = ";"
 script_dir = os.path.dirname(__file__)
-data_name = "Prod.txt"
+data_name = "Prod.csv"
 path = os.path.join(script_dir, data_name)
-
+Now = time()-timezone
+Print(Now)
 # Opening database and reading each line
 with open(path, 'r') as f:
     for line in f:
 
         # Splitting line Conso[0]=Timestamp & Conso[1]=Prod
         Conso = [float(x) for x in line.split(";")]
-        DeltaImp = (55-Conso[1]*0.6)/Conso[1]
-        for i in range(Conso[1]):
-            # OUT = 1
-            time.sleep(0.6)
-            # OUT = 0
-            time.sleep(DeltaImp)
-        while Conso[0]+60 > time.clock():
+
+        # Skip lines before now
+        if Conso[1] < Now:
+            pass
+        else:
+            # Begin prod meter emulation
+            DeltaImp = (55-Conso[1]*0.6)/Conso[1]
+            for i in range(Conso[1]):
+                GPIO.output(7, 1)  # OUT = 1
+                time.sleep(0.6)
+                GPIO.output(7, 0)  # OUT = 0
+                time.sleep(DeltaImp)
             # Wait for next minute
-            time.sleep(0.1)
+            while Conso[0]+60 > time.clock():
+                time.sleep(0.1)
